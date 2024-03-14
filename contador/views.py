fa5e58c21944ec79e376b4bc3aec5d74b625a458
Talia-goncalves <gtalia246@gmail.com>
2024-03-13 23:06:44 -0300

    feat: subindo front...

--- contador/views.py
+++ contador/views.py
@@ -1,12 +1,28 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Dados
-from .serializers import DadosSerializer
 
-@api_view(['POST'])
 def receber_dados(request):
-    serializer = DadosSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+        dados = Dados.objects.create(
+            sensor=data.get('Sensor'),
+            botao=data.get('Botao'),
+            liga_robo=data.get('LigaRobo'),
+            reset_contador=data.get('ResetContador'),
+            valor_contagem=data.get('Valor Contagem')
+        )
+        return JsonResponse({'status': 'Dados recebidos com sucesso!'})
+    return JsonResponse({'error': 'Método não permitido'}, status=405)
+
+
+def mostrar_dados(request):
+    dados = Dados.objects.last()
+    return render(request, 'index.html', {
+        'sensor': dados.sensor,
+        'botao': dados.botao,
+        'liga_robo': dados.liga_robo,
+        'reset_contador': dados.reset_contador,
+        'valor_contagem': dados.valor_contagem
+    })
